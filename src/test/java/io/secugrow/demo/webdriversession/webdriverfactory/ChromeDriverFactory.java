b429ba9da660baa22c0ce13e06ce3cb8f6c6c1d3
boris779 <boris.wrubel@gmx.at>
2023-03-10 08:30:45 +0000

    Update from https://github.com/secugrow/java-archetype/commit/d7b3f7572d937bf7f7171bfd616ad0396be58baf

--- src/test/java/io/secugrow/demo/webdriversession/webdriverfactory/ChromeDriverFactory.java
+++ src/test/java/io/secugrow/demo/webdriversession/webdriverfactory/ChromeDriverFactory.java
@@ -11,7 +11,7 @@ public class ChromeDriverFactory extends WebDriverFactory {
         WebDriverManager.chromedriver().driverVersion(super.getWebDriverVersion()).setup();
         ChromeOptions options = new ChromeOptions();
         options.setHeadless(false);
-        options.addArguments("--remote-allow-origins=*")
+        options.addArguments("--remote-allow-origins=*");
         return new ChromeDriver(options.merge(caps));
     }
 }
